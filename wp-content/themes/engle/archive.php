b1e9911ea6b676a4803131242217a9eabedf2a14
Brad Sawicki <git@bradsawicki.com>
2017-08-24 19:30:15 +0200

    Add pagination to archive tpl

--- wp-content/themes/engle/archive.php
+++ wp-content/themes/engle/archive.php
@@ -54,15 +54,11 @@
 
 		</div>
 
-    <div class="pager">
-      <ul>
-        <li class="is-current"><a href="">1</a></li>
-        <li><a href="">2</a></li>
-        <li><a href="">3</a></li>
-        <li><a href="">4</a></li>
-        <li class="pager__direction"><a href="">></a></li>
-      </ul>
-    </div>
+    <?php
+      if (function_exists("pagination")) {
+        pagination($post->max_num_pages);
+      }
+    ?>
 
   </div>
 </section>
