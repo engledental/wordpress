d3f26c833da8cd762b09be32f81b787fa7edf721
Brad Sawicki <git@bradsawicki.com>
2017-09-11 17:41:55 +0200

    GA code

--- wp-content/themes/engle/header.php
+++ wp-content/themes/engle/header.php
@@ -6,6 +6,16 @@
 	<meta content="width=device-width, initial-scale=1.0, user-scalable=1, minimum-scale=1.0" name="viewport">
 	<?php get_template_part( 'partials/favicons' ); ?>
 	<?php wp_head(); ?>
+	<!-- Google Analytics -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-69136342-3', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- End Google Analytics -->
 </head>
 <body <?php body_class(); ?>>
 	<?php get_template_part( 'partials/svg' ); ?>
