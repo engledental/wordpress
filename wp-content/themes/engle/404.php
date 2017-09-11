e06dc7460b77416e1fa6a51965be2e6675359ff7
Brad Sawicki <git@bradsawicki.com>
2017-09-11 16:09:31 +0200

    404 page markup/styles

--- wp-content/themes/engle/404.php
+++ wp-content/themes/engle/404.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
-	<h1>Whoops!</h1>
+<section class="global-page">
+	<div class="container container--narrow">
+
+		<h1 class="title__h1">Page Not Found</h1>
+
+		<div class="body-content">
+			<p>Head back home and try again.</p>
+		</div>
+
+		<p><a href="/" class="btn btn--outline">Home</a></p>
+
+	</div>
+</section>
 
 <?php get_footer(); ?>
